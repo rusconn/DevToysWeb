e2a558f9e93b529a11e8af188cf66aa92a6d258c
rusconn <rusconn3@gmail.com>
2023-06-25 01:08:21 +0900

    chore(dev): add cva to target of eslint-plugin-tailwindcss

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,7 +4,7 @@ import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/style";
 
 export const buttonVariants = cva(
-  "inline-flex items-center justify-center transition-colors disabled:opacity-50 disabled:pointer-events-none",
+  "inline-flex items-center justify-center transition-colors disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -13,7 +13,7 @@ export const buttonVariants = cva(
         ghost: "hover:bg-accent",
       },
       size: {
-        default: "h-9 rounded-md py-2 px-3",
+        default: "h-9 rounded-md px-3 py-2",
       },
     },
     defaultVariants: {
