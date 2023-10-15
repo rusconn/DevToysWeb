8fadf783b6aa89cd023ab701c81dc846d8f52c45
rusconn <rusconn3@gmail.com>
2023-10-16 00:51:20 +0900

    test: use Docker for playwright tests

--- tests/app/page.spec.ts
+++ tests/app/page.spec.ts
@@ -1,10 +1,6 @@
 import { expect, test } from "@playwright/test";
 
-// avoid VRT on CI because of difference between CI and local test env
-// we can do VRT on CI if dockerize local test env
-if (!process.env.CI) {
-  test("VRT", async ({ page }) => {
-    await page.goto("/");
-    await expect(page).toHaveScreenshot();
-  });
-}
+test("VRT", async ({ page }) => {
+  await page.goto("/");
+  await expect(page).toHaveScreenshot();
+});
