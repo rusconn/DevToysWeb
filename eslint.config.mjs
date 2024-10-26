00726365d6b7b6b5b202b12d95fd9bc8328a628e
rusconn <rusconn3@gmail.com>
2024-10-26 09:10:28 +0900

    refactor(dev)!: ESLint Flat Config

---
+++ eslint.config.mjs
@@ -0,0 +1,67 @@
+import { fixupConfigRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import reactCompiler from "eslint-plugin-react-compiler";
+import tailwind from "eslint-plugin-tailwindcss";
+import tseslint from "typescript-eslint";
+
+const flatCompat = new FlatCompat();
+
+export default [
+  {
+    ignores: [
+      "dist",
+      ".cache",
+      "public",
+      "node_modules",
+      "out",
+      "test-results/",
+      "playwright-report/",
+      "playwright/.cache/",
+    ],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  ...tailwind.configs["flat/recommended"],
+  ...fixupConfigRules(flatCompat.extends("next/core-web-vitals")),
+  prettier,
+  {
+    plugins: {
+      "react-compiler": reactCompiler,
+    },
+    settings: {
+      tailwindcss: {
+        callees: ["cn", "cva"],
+        config: "./tailwind.config.js",
+      },
+      next: {
+        rootDir: ["./"],
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "import/no-extraneous-dependencies": [
+        "error",
+        {
+          devDependencies: ["next.config.ts", "tests/**", "playwright.config.ts"],
+        },
+      ],
+      "react-compiler/react-compiler": "error",
+    },
+  },
+];
